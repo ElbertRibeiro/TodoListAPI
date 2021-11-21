debf194a6e2fe688d9e4275322325066e5ebbd33
elbertribeiro <elbertprofissional@gmail.com>
2021-11-21 17:41:35 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+let express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
@@ -7,18 +7,14 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
-
+mongoose.connect('mongodb://localhost/Tododb').then(() => console.log('MongoDB connected...'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-var routes = require('./api/routes/todoListRoutes'); //importing route
+let routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
-
 
 app.listen(port);
 
-
-console.log('todo list RESTful API server started on: ' + port);
+console.log('TodoListAPI RESTFull API server started on: ' + port);
